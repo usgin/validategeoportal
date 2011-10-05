1b4509c9792478c54bf542fabbf1aef555ecc348
Ryan Clark <ryan.clark.j@gmail.com>
2011-10-05 11:06:00 -0700

    Made the logger write the report as it goes, so that if the process is aborted we still get SOME results.

--- geoportalvalid/logger.py
+++ geoportalvalid/logger.py
@@ -2,7 +2,9 @@ import csv
 from config import reportFilePath
 
 failed_results = dict()
-
+logWriter = csv.writer(open(reportFilePath, "wb"))
+logWriter.writerow([ "DOCUUID", "XPATH", "VALUE", "MESSAGE" ])
+    
 class LogEntry():
     def __init__(self, xpath, value, message):
         self.xpath = xpath
@@ -12,21 +14,17 @@ class LogEntry():
 def log(valid, recordId, xpath, value, message=""):
     if not valid:
         log_entry = LogEntry(xpath, value, message)
+        logWriter.writerow([ id, log_entry.xpath, log_entry.value, log_entry.message ])
+        
         if recordId in failed_results:
             failed_results[recordId].append( log_entry )
         else:
             failed_results[recordId] = [ log_entry ]
             
 def writeout():
-    logWriter = csv.writer(open(reportFilePath, "wb"))
-    logWriter.writerow([ "DOCUUID", "XPATH", "VALUE", "MESSAGE" ])
-    
     fail_count = 0
     for id in failed_results:
-        for entry in failed_results[id]:
-            fail_count = fail_count + 1
-            
-            logWriter.writerow([ id, entry.xpath, entry.value, entry.message ])
+        fail_count = fail_count + len(failed_results[id])
             
     print str(len(failed_results)) + " metadata records failed one or more validation rules."        
     print str(fail_count) + " records in the log."
